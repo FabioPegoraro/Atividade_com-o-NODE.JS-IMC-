24ccd4d07d08d6a72796adcc6b58d52587a3c86e
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-06-19 14:23:14 -0300

    Detalhamneto de todas os IMC

--- meuscript.js
+++ meuscript.js
@@ -1,13 +1,22 @@
 let rs = require("readline-sync")
 
-let peso = rs.question("Qual o seu peso?")
+let nome = rs.question("Ola, qual o seu nome?\n")
 
-let altura = rs.questionFloat("Qual a sua altura?")
+let peso = rs.question(nome + ", qual o seu peso? (Ex.: 80)\n")
+
+let altura = rs.questionFloat("Qual a sua altura?  (Ex.: 1.70)\n")
 
 let imc = peso / (altura*altura)
 
-console.log("O seu IMC é " + imc)
-
-
-
+if (imc <18.5) {
+   console.log (nome + " seu IMC é " + imc + ", sua classificação é Magreza (Abaixo do Peso Ideial)\n")
+} else if (imc>=18.5 && imc <24.9) {
+    console.log (nome + " seu IMC é " + imc + ", sua classificação é Normal (Peso Ideal)\n")
+} else if (imc>=25 && imc <29.9) {
+    console.log (nome + " seu IMC é " + imc + ", sua classificação é Obesidade I (Sobre Peso)\n")
+} else if (imc>=30 && imc <39.9) {
+    console.log (nome + " seu IMC é " + imc + ", sua classificação é Obesidade II (Severa)\n")
+} else if (imc>40) {
+    console.log (nome + " seu IMC é " + imc + ", sua classificação é Obesidade III(Morbida)\n")
+}
 
